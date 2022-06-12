3c2fb06df0ce75cd7d4f915598a5dac69e97eae8
Thalía Mariana <thaliamariana@live.com>
2022-06-12 19:15:58 -0300

    melhorias no projeto

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
-import  { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat'
-import Cesta from "./src/telas/Cesta";
+import  { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
 
@@ -11,13 +13,13 @@ export default function App() {
   });
 
   if (!fonteCarregada) {
-    return <SafeAreaView />
+    return <AppLoading />
   }
 
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
